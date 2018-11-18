b2b1caaeeecd76bae8b0ad1eea1c38046b654408
g i b <Gibby@Aether.local>
2018-11-17 23:37:26 -1000

    isUnique

---
+++ Arrays and Strings/isUnique.java
@@ -0,0 +1,19 @@
+
+// Time Complexity  :  O(n) where n is the length of the string
+// Space Complexity :  O(1) we're always dealing with 128 possible ASCII values.
+
+public class isUnique {
+
+    public static boolean isUnique(String s){
+        boolean[] table = new boolean[128];
+        for(int i = 0; i < s.length(); i++) {
+            int c = s.charAt(i);
+            if (table[c]) {
+                return false;
+            } else {
+                table[c] = true;
+            }
+        }
+        return true;
+    }
+}
